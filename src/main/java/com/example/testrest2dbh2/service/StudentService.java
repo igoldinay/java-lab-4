48f513b4744363287f9e68af3f18f3c37bbe3d1b
igoldinay <y_igoldina@mail.ru>
2022-11-23 21:47:31 +0500

    lab 4 done

---
+++ src/main/java/com/example/testrest2dbh2/service/StudentService.java
@@ -0,0 +1,17 @@
+package com.example.testrest2dbh2.service;
+
+import com.example.testrest2dbh2.entity.Student;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface StudentService {
+    List<Student> getAllStudent();
+
+    Student getStudent(int id);
+
+    Student saveStudent(Student student);
+
+    void deleteStudent(int id);
+}
